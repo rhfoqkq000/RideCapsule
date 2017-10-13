c036c4fbdabbbfa9ce30ddc271b726416d50a504
DESKTOP-DU2HSTA\ekekd <ekekdid@naver.com>
2017-10-13 17:44:48 +0900

    Weather 추가 ( 액션바 수정 필요 )

---
+++ app/src/main/java/com/example/horse/travel/tourist/FragmentSecond.java
@@ -0,0 +1,39 @@
+package com.example.horse.travel.tourist;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.horse.travel.R;
+
+/**
+ * Created by ekekd on 2017-10-09.
+ */
+
+public class FragmentSecond extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_second, container, false);
+
+        TextView tv = (TextView) v.findViewById(R.id.tvFragFirst);
+        tv.setText(getArguments().getString("msg"));
+
+        return v;
+    }
+
+    public static FragmentSecond newInstance(String text) {
+
+        FragmentSecond f = new FragmentSecond();
+        Bundle b = new Bundle();
+        b.putString("msg", text);
+
+        f.setArguments(b);
+
+        return f;
+    }
+
+}
